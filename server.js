2750d3190a0757d943541ff5882332133c3b9788
jin <jinsu.kim1011@gmail.com>
2025-11-13 23:27:48 +0100

    feat: set view engine to ejs and define view directory for rendering pages

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-
+app.set('view engine', 'ejs')
+app.set('views', './server/views/pages')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client')));
@@ -11,6 +12,22 @@ module.exports = app;
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
+app.get('/work', (req, res) => {
+    res.render('work')
+});
+app.get('/blog', (req, res) => {
+    res.render('blog')
+});
+app.get('/admin', (req, res) => {
+    res.render('admin')
+});
+app.get('/password', (req, res) => {
+    res.render('password')
+});
+app.get('/edit', (req, res) => {
+    res.render('edit')
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
