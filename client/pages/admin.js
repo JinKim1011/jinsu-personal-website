20e9a52cd177500809ad5b471d2ba61b25bb7689
jin <jinsu.kim1011@gmail.com>
2025-11-14 01:23:10 +0100

    fix: update redirect paths in admin.js to ensure correct navigation to the password page

--- client/pages/admin.js
+++ client/pages/admin.js
@@ -1,10 +1,11 @@
-
 document.addEventListener('DOMContentLoaded', () => {
+    if (window.__IS_ADMIN__ === true) return;
+
     try {
         if (sessionStorage.getItem('isAdmin') !== 'true') {
-            window.location.href = 'password.html'
+            window.location.href = '/password';
         }
     } catch (e) {
-        window.location.href = 'password.html'
+        window.location.href = '/password';
     }
 });
