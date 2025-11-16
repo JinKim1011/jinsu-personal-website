f9b56c56a5b3b967379d2edce27e1d8c80804a17
jin <jinsu.kim1011@gmail.com>
2025-11-16 19:45:07 +0100

    feat: protect create, update, delete with password middleware

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -7,4 +7,13 @@ router.post('/content', passwordGate, (req, res) => {
     res.json({ ok: true });
 });
 
+// Create
+router.post('/posts', passwordGate, async (req, res) => { });
+
+// Update
+router.post('/posts/:slug', passwordGate, async (req, res) => { });
+
+// Delete (link-based GET)
+router.get('/posts/:slug/delete', passwordGate, async (req, res) => { });
+
 module.exports = router;
